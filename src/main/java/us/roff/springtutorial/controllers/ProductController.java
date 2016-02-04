738a415c520cc1fbce8f3f6647c0aa4010c61595
Ron Roff <rroff@roff.us>
2016-02-04 16:53:44 -0500

    Added capability to view details of a product using an MVC architecture.

--- src/main/java/us/roff/springtutorial/controllers/ProductController.java
+++ src/main/java/us/roff/springtutorial/controllers/ProductController.java
@@ -3,6 +3,7 @@ package us.roff.springtutorial.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import us.roff.springtutorial.services.ProductService;
@@ -23,5 +24,14 @@ public class ProductController {
 		model.addAttribute("products", productService.listAllProducts());
 		
 		return "products";
+	}
+	
+	@RequestMapping("/product/{id}")
+	public String getProduct(@PathVariable Integer id, Model model) {
+		
+		model.addAttribute("product", productService.getProductById(id));
+		
+		return "product";
+		
 	}
 }
--- src/main/java/us/roff/springtutorial/services/ProductService.java
+++ src/main/java/us/roff/springtutorial/services/ProductService.java
@@ -7,4 +7,6 @@ import us.roff.springtutorial.domain.Product;
 public interface ProductService {
 
 	List<Product> listAllProducts();
+	
+	Product getProductById(Integer id);
 }
