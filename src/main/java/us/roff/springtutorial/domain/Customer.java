01efb7b44457cc4da2986fa92a98d5a693d8e31d
Ron Roff <rroff@roff.us>
2016-02-10 16:35:20 -0500

    Added version property to Entities.

--- src/main/java/us/roff/springtutorial/domain/Customer.java
+++ src/main/java/us/roff/springtutorial/domain/Customer.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Version;
 
 @Entity
 public class Customer implements DomainObject {
@@ -11,6 +12,10 @@ public class Customer implements DomainObject {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
+	
+	@Version
+	private Integer version;
+	
 	private String firstName;
 	private String lastName;
 	private String emailAddress;
@@ -29,6 +34,14 @@ public class Customer implements DomainObject {
 	@Override
 	public void setId(Integer id) {
 		this.id = id;
+	}
+	
+	public Integer getVersion() {
+		return version;
+	}
+	
+	public void setVersion(Integer version) {
+		this.version = version;
 	}
 	
 	public String getFirstName() {
--- src/main/java/us/roff/springtutorial/domain/Product.java
+++ src/main/java/us/roff/springtutorial/domain/Product.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Version;
 
 @Entity
 public class Product implements DomainObject {
@@ -13,6 +14,10 @@ public class Product implements DomainObject {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
+	
+	@Version
+	private Integer version;
+	
 	private String description;
 	private BigDecimal price;
 	private String imageUrl;
@@ -25,6 +30,14 @@ public class Product implements DomainObject {
 	@Override
 	public void setId(Integer id) {
 		this.id = id;
+	}
+	
+	public Integer getVersion() {
+		return version;
+	}
+	
+	public void setVersion(Integer version) {
+		this.version = version;
 	}
 	
 	public String getDescription() {
