e221d37a068e7cfcba163092a596be5e34ba4382
Ron Roff <rroff@roff.us>
2016-02-16 15:14:23 -0500

    Added Cart & CartDetail domain classes to illustrate @OneToMany and @ManyToOne JPA annotations.

---
+++ src/main/java/us/roff/springtutorial/domain/CartDetail.java
@@ -0,0 +1,60 @@
+package us.roff.springtutorial.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Version;
+
+@Entity
+public class CartDetail implements DomainObject {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@Version
+	private Integer version;
+	
+	@ManyToOne
+	private Cart cart;
+	
+	@OneToOne
+	private Product product;
+	
+	@Override
+	public Integer getId() {
+		return id;
+	}
+	
+	@Override
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public Integer getVersion() {
+		return version;
+	}
+	
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+
+	public Cart getCart() {
+		return cart;
+	}
+	
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	}
+	
+	public Product getProduct() {
+		return product;
+	}
+	
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+}
--- src/test/java/us/roff/springtutorial/services/UserServiceJpaDaoImplTest.java
+++ src/test/java/us/roff/springtutorial/services/UserServiceJpaDaoImplTest.java
@@ -9,10 +9,15 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import us.roff.springtutorial.config.JpaIntegrationConfig;
+import us.roff.springtutorial.domain.Cart;
+import us.roff.springtutorial.domain.CartDetail;
 import us.roff.springtutorial.domain.Customer;
+import us.roff.springtutorial.domain.Product;
 import us.roff.springtutorial.domain.User;
 
 import static org.junit.Assert.*;
+
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(JpaIntegrationConfig.class)
@@ -21,9 +26,16 @@ public class UserServiceJpaDaoImplTest {
 	
 	private UserService userService;
 	
+	private ProductService productService;
+		
 	@Autowired
 	public void setUserService(UserService userService) {
 		this.userService = userService;
+	}
+	
+	@Autowired
+	public void setProductService(ProductService productService) {
+		this.productService = productService;
 	}
 
 	@Test
@@ -67,5 +79,91 @@ public class UserServiceJpaDaoImplTest {
 		assertEquals(0, savedUser.getVersion().intValue());
 		assertNotNull(savedUser.getCustomer());
 		assertNotNull(savedUser.getCustomer().getId());
+	}
+	
+	@Test
+	@DirtiesContext
+	public void testNewUserWithCart() throws Exception {
+		String username = "someuser";
+		String password = "password";
+		
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(password);
+		
+		user.setCart(new Cart());
+		
+		User savedUser = userService.saveOrUpdate(user);
+		assertNotNull(savedUser.getId());
+		assertEquals(0, savedUser.getVersion().intValue());
+		assertNotNull(savedUser.getCart());
+		assertNotNull(savedUser.getCart().getId());
+	}
+	
+	@Test
+	@DirtiesContext
+	public void testNewUserWithCartDetails() throws Exception {
+		String username = "someuser";
+		String password = "password";
+		
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(password);
+		
+		user.setCart(new Cart());
+		
+		List<Product> products = (List<Product>)productService.listAll();
+		
+		CartDetail cartItem = new CartDetail();
+		cartItem.setProduct(products.get(0));
+		user.getCart().addCartDetail(cartItem);
+		
+		cartItem = new CartDetail();
+		cartItem.setProduct(products.get(1));
+		user.getCart().addCartDetail(cartItem);
+		
+		User savedUser = userService.saveOrUpdate(user);
+		assertNotNull(savedUser.getId());
+		assertEquals(0, savedUser.getVersion().intValue());
+		
+		Cart cart = savedUser.getCart();
+		assertNotNull(cart);
+		assertEquals(2, cart.getCartDetails().size());
+	}
+	
+	@Test
+	@DirtiesContext
+	public void testAddAndRemoveCartDetailsToUser() throws Exception {
+		String username = "someuser";
+		String password = "password";
+		
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(password);
+		
+		user.setCart(new Cart());
+		
+		List<Product> products = (List<Product>)productService.listAll();
+		
+		CartDetail cartItem = new CartDetail();
+		cartItem.setProduct(products.get(0));
+		user.getCart().addCartDetail(cartItem);
+		
+		cartItem = new CartDetail();
+		cartItem.setProduct(products.get(1));
+		user.getCart().addCartDetail(cartItem);
+		
+		User savedUser = userService.saveOrUpdate(user);
+		assertNotNull(savedUser.getId());
+		assertNotNull(savedUser.getCart());
+		assertEquals(2, savedUser.getCart().getCartDetails().size());
+		
+		savedUser.getCart().removeCartDetail(
+				savedUser.getCart().getCartDetails().get(0));
+		
+		User modifiedUser = userService.saveOrUpdate(savedUser);
+		assertNotNull(modifiedUser.getId());
+		assertNotNull(modifiedUser.getCart());
+		assertEquals(1, modifiedUser.getCart().getCartDetails().size());
 	}
 }
